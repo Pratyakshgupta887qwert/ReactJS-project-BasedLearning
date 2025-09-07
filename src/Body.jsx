00d2f537ac7d23e5998dfa857d44a3834acf8774
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2025-09-07 19:05:22 +0530

    updation

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Viaemailform from './Viaemailform';
+import Submit from './Submit';
 function Body(){
     return(
         <>
@@ -11,10 +12,13 @@ function Body(){
         <div className='flex ml-30'>
             
             <form className='mt-20'>
-            <button className='text-white bg-black pl-5 pr-5 pt-2 pb-2 rounded-sm'>VIA SUPPORT CHAT</button>
-            <button className='text-white bg-black pl-5 pr-5 pt-2 pb-2 ml-10 w-50 rounded-sm'>VIA CALL</button>
+            <button className='text-white bg-black pl-5 pr-8 pt-5 pb-5 rounded-sm font-bold '>VIA SUPPORT CHAT</button>
+            <button className='text-white bg-black pl-5 pr-8 pt-5 pb-5 ml-10 w-50 rounded-sm font-bold'>VIA CALL</button>
             <br></br>
             <Viaemailform/>
+            
+            <br></br>
+            <Submit/>
             </form>
             <img className='h-150 mt-8 ml-90' src='Screenshot 2025-09-07 153441.png'></img>
             
---
+++ src/Submit.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function Submit(){
+    return(
+        <>
+        <button className='text-white bg-black pl-20 pr-20 pt-3 pb-3 rounded-sm font-bold mt-5'>SUBMIT</button>
+        </>
+    )
+}
+export default Submit;
