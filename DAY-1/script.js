a1559aec25fa11a55bc1a66348df77672ee861b9
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2025-09-08 22:23:08 +0530

    adding data

---
+++ DAY-1/script.js
@@ -0,0 +1,32 @@
+document.write("i am pratyaksh <br>")
+document.write("i am a developer <br>")
+
+document.write("ram love coding <br>")
+document.write("ram love coding <br>")
+
+//variables : it is use to create a box like thing which can be use anytime and anywhere.
+let myname = "payal"
+
+document.write(myname+"love coding <br>");
+document.write(myname+"love coding <br>");
+document.write(myname+"love coding <br>");
+document.write(myname+"love coding <br>");
+
+
+console.log("hello world")
+
+
+let num1 = 100
+let num2 = 30
+
+//operators : airthmetic operators
+
+let addition = num1+num2
+let substraction = num1-num2
+let multipication = num1*num2
+let division = num1/num2
+
+document.writeln("Addition of 2 numbers ="+ addition+"<br>");
+document.writeln("Substraction of 2 numbers ="+ substraction+"<br>");
+document.writeln("Multipication of 2 numbers ="+ multipication+"<br>");
+document.writeln("Division of 2 numbers ="+ division+"<br>");
