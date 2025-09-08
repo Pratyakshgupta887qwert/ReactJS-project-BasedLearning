ab53fc7becb70550b35ed3bc323de680bab3738d
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2025-09-08 20:11:02 +0530

    deleting all

--- src/Buttons.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-function Buttons(props){
-    return(
-        <>
-        <button className='cursor-pointer'>{props.label}</button>
-        </>
-    )
-}
-export default Buttons;
--- src/Submit.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-function Submit(){
-    return(
-        <>
-        <button className='cursor-pointer text-white bg-black pl-20 pr-20 pt-3 pb-3 rounded-sm font-bold mt-5'>SUBMIT</button>
-        </>
-    )
-}
-export default Submit;
